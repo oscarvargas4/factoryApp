bef980bb679e1c1759dbbbe777e481304b366be6
Oscar <oscar.andres.vargas1@gmail.com>
2022-01-14 21:32:47 +0100

    postman

--- src/app.js
+++ src/app.js
@@ -19,7 +19,6 @@ app.get('/', (req, res) => {
 });
 
 // Routes
-app.use('/players', require('./routes/players'));
 app.use('/predefineValues', require('./routes/predefineValues'));
 app.use('/cars', require('./routes/cars'));
 app.use('/order', require('./routes/orders'));
