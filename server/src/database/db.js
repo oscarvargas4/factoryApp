9bf3f801465bba31fae617721762614b327814af
Oscar <oscar.andres.vargas1@gmail.com>
2022-01-17 10:38:37 +0100

    MSSQL stablished

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -1,13 +1,31 @@
 const { Sequelize } = require("sequelize");
 const { database } = require("../../ecosystem.config");
 
+// MySQL
+// const sequelize = new Sequelize(
+//   database.database,
+//   database.username,
+//   database.password,
+//   {
+//     host: database.host,
+//     dialect: "mysql",
+//   }
+// );
+
+// Microsoft SQL Server
 const sequelize = new Sequelize(
   database.database,
   database.username,
   database.password,
   {
+    dialect: 'mssql',
     host: database.host,
-    dialect: "mysql",
+    port: database.port, // Default port
+    logging: false, // disable logging; default: console.log
+
+    dialectOptions: {
+      requestTimeout: 30000, // timeout = 30 seconds
+    },
   }
 );
 
